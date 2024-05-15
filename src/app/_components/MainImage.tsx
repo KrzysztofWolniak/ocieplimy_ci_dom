d328e64ef2b22718929e1d2e98f978196e008017
KrzysztofWolniak <261288@student.pwr.edu.pl>
2024-05-15 08:13:53 +0200

    test

--- src/app/_components/MainImage.tsx
+++ src/app/_components/MainImage.tsx
@@ -3,5 +3,5 @@ import { Image } from "@nextui-org/react";
 import mainImage from "./assets/attic.webp";
 import NextImage from "next/image";
 export default function MainImage() {
-  return <NextImage  src={mainImage} alt="NextUI Album Cover" className="" />;
+  return <NextImage  src={mainImage} alt="NextUI Album Cover" className="" />; 
 }
