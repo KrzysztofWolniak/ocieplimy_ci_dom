3dbd1f50d321fb0df02f002e8444edbcb929056b
KrzysiekWolniak <krzysztof.wolniak@capgemini.com>
2024-06-28 15:19:10 +0200

    changed to mapped component

--- src/app/components/DropdownComponent.tsx
+++ src/app/components/DropdownComponent.tsx
@@ -1,65 +1,46 @@
 import React from "react";
-import {
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownItem,
-  Button,
-} from "@nextui-org/react";
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, DropdownSection, Button } from "@nextui-org/react";
+import Link from "next/link";
 
-export default function DropdownComponent() {
-  const items = [
-    {
-      key: "new",
-      label: "New file",
-    },
-    {
-      key: "copy",
-      label: "Copy link",
-    },
-    {
-      key: "edit",
-      label: "Edit file",
-    },
-    {
-      key: "delete",
-      label: "Delete file",
-    },
-  ];
-
-  return (
-    <Dropdown backdrop="blur">
-      <DropdownTrigger>
-        <Button variant="light" className="text-white lg:hidden flex-col h-full">
-          <svg
-            className="h-6 w-6"
-            stroke="currentColor"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              className="inline-flex"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16m-7 6h7"
-            />
-          </svg>
-          Menu
-        </Button>
-      </DropdownTrigger>
-      <DropdownMenu aria-label="Dynamic Actions"  items={items}>
-        {(item) => (
-          <DropdownItem
-            key={item.key}
-            color={item.key === "delete" ? "danger" : "default"}
-            className={item.key === "delete" ? "text-danger" : ""}
-            
-          >
-            {item.label}
-          </DropdownItem>
-        )}
-      </DropdownMenu>
-    </Dropdown>
-  );
+export default function DropdownComponent({ navbarItems }) {
+	return (
+		<Dropdown backdrop="blur">
+			<DropdownTrigger>
+				<Button variant="light" className="text-white lg:hidden flex-col h-full">
+					<svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+						<path className="inline-flex" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+					</svg>
+					Menu
+				</Button>
+			</DropdownTrigger>
+			<DropdownMenu aria-label="Dynamic Actions">
+				{navbarItems.map((item) =>
+					item.submenu.length > 0 ? (
+						<DropdownSection key={item.text} title={item.text} showDivider>
+							{item.submenu.map((submenuItem) => (
+								<DropdownItem
+									key={submenuItem.key}
+									as={Link}
+									href={submenuItem.href}
+									description={submenuItem.description}
+									startContent={submenuItem.icon}
+								>
+									{submenuItem.text}
+								</DropdownItem>
+							))}
+						</DropdownSection>
+					) : (
+						<DropdownItem key={item.text} as={Link} href={item.href}>
+							{item.text}
+						</DropdownItem>
+					)
+				)}
+				<DropdownSection title="Danger zone">
+					<DropdownItem key="delete" className="text-White" color="warning" as={Link} href="/contact" isSelected>
+						Napisz Do Nas
+					</DropdownItem>
+				</DropdownSection>
+			</DropdownMenu>
+		</Dropdown>
+	);
 }
